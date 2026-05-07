9faef92ea97220d713d2c03dc54c11a9a29d7b51
moh dikrullah <mmohdikrullah18@gmail.com>
2026-05-07 19:10:54 +0700

    refactor: update LocalDbService untuk mendukung penyimpanan data umum (JSON)

--- lib/services/local_db_service.dart
+++ lib/services/local_db_service.dart
@@ -3,15 +3,28 @@ import '../models/schedule_model.dart';
 
 class LocalDbService {
   static const String scheduleBoxName = 'schedulesBox';
+  static const String appBoxName = 'appBox';
 
   static Future<void> init() async {
     await Hive.initFlutter();
     Hive.registerAdapter(ScheduleModelAdapter());
     await Hive.openBox<ScheduleModel>(scheduleBoxName);
+    await Hive.openBox(appBoxName);
   }
 
   static Box<ScheduleModel> get scheduleBox => Hive.box<ScheduleModel>(scheduleBoxName);
+  static Box get appBox => Hive.box(appBoxName);
 
+  // Generic Storage Methods
+  static Future<void> saveData(String key, dynamic data) async {
+    await appBox.put(key, data);
+  }
+
+  static dynamic getData(String key) {
+    return appBox.get(key);
+  }
+
+  // Schedule Specific
   static Future<void> addSchedule(ScheduleModel schedule) async {
     await scheduleBox.put(schedule.id, schedule);
   }
